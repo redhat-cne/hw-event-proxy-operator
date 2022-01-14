2de580163d8ef99cbf6805f47c1468e2719551c9
Jack Ding <jacding@redhat.com>
2022-01-14 16:40:04 -0500

    Update tools path for ci
    Signed-off-by: Jack Ding <jacding@redhat.com>

--- hack/install-integration-tests-deps.sh
+++ hack/install-integration-tests-deps.sh
@@ -5,15 +5,15 @@ set -e
 _GOOS=$(go env GOOS)
 _GOARCH=$(go env GOARCH)
 _ARCH=$(arch)
-
-mkdir -p ./bin
+PATH_TOOLS=./bin
+mkdir -p ${PATH_TOOLS}
 
 # Install kubectl if needed.
 if ! [ -x "$(command -v kubectl)" ]; then
     echo "Installing kubectl..."
     curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/$_GOOS/$_GOARCH/kubectl
-    mv ./kubectl ./bin/
-    chmod 755 ./bin/kubectl
+    mv ./kubectl ${PATH_TOOLS}
+    chmod +x ${PATH_TOOLS}/kubectl
 else
     echo "No need to install kubectl, continue..."
 fi
@@ -22,9 +22,8 @@ fi
 if ! [ -x "$(command -v kubectl-kuttl)" ]; then
     echo "Installing kubectl-kuttl..."
     curl -LO https://github.com/kudobuilder/kuttl/releases/download/v0.11.0/kubectl-kuttl_0.11.0_${_GOOS}_${_ARCH}
-    mv ./kubectl-kuttl_0.11.0_${_GOOS}_${_ARCH} ./bin/kubectl-kuttl
-    ls ./bin/
-    chmod +x ./bin/kubectl-kuttl
+    mv ./kubectl-kuttl_0.11.0_${_GOOS}_${_ARCH} ${PATH_TOOLS}/kubectl-kuttl
+    chmod +x ${PATH_TOOLS}/kubectl-kuttl
 else
     echo "No need to install kubectl-kuttl, continue..."
 fi
