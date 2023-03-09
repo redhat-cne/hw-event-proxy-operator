6027cdfd490c37253724ed551147f0dfe1bbf423
Jack Ding <jackding@gmail.com>
2023-03-09 15:57:53 -0500

    mount cloud-event-proxy store to pvc for http
    Signed-off-by: Jack Ding <jackding@gmail.com>

--- api/v1alpha1/groupversion_info.go
+++ api/v1alpha1/groupversion_info.go
@@ -15,8 +15,8 @@ limitations under the License.
 */
 
 // Package v1alpha1 contains API Schema definitions for the event v1alpha1 API group
-//+kubebuilder:object:generate=true
-//+groupName=event.redhat-cne.org
+// +kubebuilder:object:generate=true
+// +groupName=event.redhat-cne.org
 package v1alpha1
 
 import (
